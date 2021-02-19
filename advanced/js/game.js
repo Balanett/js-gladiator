62606d5e91e693acca04e92141ebb22dee094ac9
Balázs Anett <71498781+Balanett@users.noreply.github.com>
2021-02-19 22:03:16 +0100

    Gladiator's fight advanced

--- advanced/js/game.js
+++ advanced/js/game.js
@@ -1,9 +1,27 @@
 const form = document.querySelector("form")
 const chooseBtn = document.getElementById("chooseBtn")
+const chosen = []
+const opponent = []
 
 chooseBtn.addEventListener("click", event => {
     event.preventDefault();
-    let chosenGlad = form.glad.value
-    let chosenWeapon = form.weap.value
-    console.log(chosenGlad, chosenWeapon)
+
+    const chosenGlad = form.glad.value
+    const chosenWeapon = form.weap.value
+    chosen.push(chosenGlad,chosenWeapon)
+
+    const notChosenGlads = []
+    const GladClass = document.getElementsByClassName("glad")
+    for (let i=0; i<GladClass.length; i++) {
+        if (GladClass[i].checked === false)
+        notChosenGlads.push(GladClass[i].value)
+    }
+
+    const GladOptions = notChosenGlads;
+    const Weapons = ["Trident", "Spear", "Club"]
+    const randomGlad = Math.floor(Math.random() * GladOptions.length);
+    const randomWeap = Math.floor(Math.random() * Weapons.length);
+    opponent.push(GladOptions[randomGlad], Weapons[randomWeap]);
+
+    console.log(chosen, opponent)
 })
---
+++ advanced/js/start.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById("startBtn")
+
+startBtn.addEventListener("click", event => {
+    event.preventDefault();
+    window.location = "game.html"
+})
